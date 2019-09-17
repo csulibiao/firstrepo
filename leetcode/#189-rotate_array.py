e630222df208f9fa5982bf112abb02c80ea7ad95
csulibiao <libiaozhijia@gmail.com>
2019-09-17 22:35:14 +0800

    update code

---
+++ leetcode/#189-rotate_array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, nums, k):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k=k%len(nums)
+        nums[:k],nums[k:]=nums[len(nums)-k:],nums[:len(nums)-k]
+      
+
+
+sol = Solution()
+print(sol.rotate([1,2,3,4,5,6],9))
+ 
