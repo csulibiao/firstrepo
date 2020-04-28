36e63e332691bada70cd6fbb4678233da83cec7e
csulibiao <libiaozhijia@gmail.com>
2020-04-28 20:54:34 +0800

    update token

--- practice/www.py
+++ practice/www.py
@@ -1,4 +1,4 @@
 ## convert www.zhidao.baidu.com to com/baidu/zhidao/www"
-token = c95c7322ebcd644e7ba4b875dbd7bc3cbb369ef2
+token = '677a67d1cc97ad2a2f2a70ecd1a61a6a84bb86e0'
 astr='www.zhidao.baidu.com'
 print('/'.join(astr.split('.')[::-1]))
